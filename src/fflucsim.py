6d2469203d7cf7d22c0c8ac55295f1036680a15c
Mathieu Hénault <mathieu.henault@cuanschutz.edu>
2025-12-09 00:42:23 -0700

    Added support for trisomy opposite of monosomy, tetrasomes opposite of reversion from trisomes, and death opposite of reversion from monosomes. Added homolog specificity. Added history tracking of mutational events.

--- src/fflucsim.py
+++ src/fflucsim.py
@@ -11,100 +11,210 @@ class CellMonosome:
         self.born = gen
         self.daughters = []
         self.age = 0
-        
+        self.dead = False
+        self.history = []
+ 
         if mother != None:
-            self.monosome_fitness = mother.monosome_fitness
-            self.monosome_rate = mother.monosome_rate
-            self.revert_rate = mother.revert_rate
+            self.w_mono = mother.w_mono
+            self.w_triso = mother.w_triso
+            self.mu_A = mother.mu_A
+            self.mu_R = mother.mu_R
+            self.mu_T = mother.mu_T
             self.ploidy = mother.ploidy
             self.genealogy = f'{mother.genealogy}.1'
             self.mother = mother.uid
+            self.disome = mother.disome
             self.monosome = mother.monosome
             self.revertant = mother.revertant
+            self.revertant2 = mother.revertant2
+            self.trisome = mother.trisome
+            self.tetrasome = mother.tetrasome
             self.generation_monosome = mother.generation_monosome
             self.generation_revert = mother.generation_revert
+            self.generation_trisome = mother.generation_trisome
+            self.generation_revert2 = mother.generation_revert2
+            self.generation_tetrasome = mother.generation_tetrasome
             self.rng = mother.rng
+            self.homologs = {}
+            for (h,i) in mother.homologs.items():
+                self.homologs[h] = i
         
         else:
+            self.ploidy = 2
             self.genealogy = '0'
             self.mother = None
+            self.disome = True
             self.monosome = False
             self.revertant = False
+            self.revertant2 = False
+            self.trisome = False
+            self.tetrasome = False
             self.generation_monosome = None
             self.generation_revert = None
+            self.generation_trisome = None
+            self.generation_revert2 = None
+            self.generation_tetrasome = None
             self.rng = np.random.default_rng()
+            self.homologs = {0:1, 1:1}
 
-    def founder(self, monosome_fitness, monosome_rate, revert_rate, ploidy):
-        self.monosome_fitness = monosome_fitness
-        self.monosome_rate = monosome_rate
-        self.revert_rate = revert_rate
-        self.ploidy = ploidy
+    def founder(self, w_mono, w_triso, mu_A, mu_R, mu_T):
+        self.w_mono = w_mono
+        self.w_triso = w_triso
+        self.mu_A = mu_A
+        self.mu_R = mu_R
+        self.mu_T = mu_T
         
         return self
+    
+    def get_homologs(self):
+        return np.concatenate([np.repeat(h,i) for (h,i) in self.homologs.items()])
+
+    def selection(self):
+        return self.get_homologs().sum == 0
         
-    def become_monosome(self, gen):
+    def become_monosome(self, gen, h=None):
+        self.disome = False
         self.monosome = True
         self.ploidy -= 1
         self.generation_monosome = gen
+        if h == None:
+            h = self.rng.choice(self.get_homologs(), 1)[0]
+        self.homologs[h] -= 1
+        return h
         
-    def become_revert(self, gen):
-        self.revertant = True
+    def become_trisome(self, gen, h=None):
+        self.disome = False
+        self.trisome = True
+        self.ploidy += 1
+        self.generation_trisome = gen
+        if h == None:
+            h = self.rng.choice(self.get_homologs(), 1)[0]
+        self.homologs[h] += 1
+        return h
+        
+    def become_revert(self, gen, h=None):
         self.monosome = False
+        self.revertant = True
+        self.disome = True
         self.ploidy += 1
         self.generation_revert = gen
+        if h == None:
+            h = self.rng.choice(self.get_homologs(), 1)[0]
+        self.homologs[h] += 1
+        return h
+    
+    def become_revert2(self, gen, h=None):
+        self.trisome = False
+        self.revertant2 = True
+        self.disome = True
+        self.ploidy -= 1
+        self.generation_revert2 = gen
+        if h == None:
+            h = self.rng.choice(self.get_homologs(), 1)[0]
+        self.homologs[h] -= 1
+        return h
+
+    def become_tetrasome(self, gen, h):
+        self.trisome = False
+        self.tetrasome = True
+        self.revertant2 = False
+        self.ploidy += 1
+        self.generation_tetrasome = gen
+        self.homologs[h] += 1
 
     def decide_divide(self):
-        if self.monosome:
-            return self.rng.random() < self.monosome_fitness
+        if self.dead == False:
+            if self.monosome:
+                return self.rng.random() < self.w_mono
+            elif self.trisome or self.tetrasome:
+                return self.rng.random() < self.w_triso
+            else:
+                return True
         else:
-            return True
+            return False
 
     def decide_monosome(self):
-        return self.rng.random() < self.monosome_rate
+        return self.rng.random() < self.mu_A
 
     def decide_revert(self):
-        return self.rng.random() < self.revert_rate
+        return self.rng.random() < self.mu_R
+
+    def decide_revert2(self):
+        return self.rng.random() < self.mu_T
 
     def divide(self, daughter_uid, gen):
         daughter = CellMonosome(daughter_uid, gen, self)
 
         event_monosome = False
         event_revert = False
-        
+        event_revert2 = False
+    
         self.age += 1
         self.daughters.append(daughter_uid)
         self.genealogy = f'{self.genealogy}.0'
         
-        if not self.monosome:
+        if self.disome:
             if self.decide_monosome():
-                daughter.become_monosome(gen)
+                h = daughter.become_monosome(gen)
+                self.become_trisome(gen, h)
                 event_monosome = True
-        else:
+
+        elif self.monosome:
             if self.decide_revert():
-                daughter.become_revert(gen)
+                h = daughter.become_revert(gen)
+                self.dead = True
                 event_revert = True
-            
-        return daughter, event_monosome, event_revert
         
-    def summary(self):
-        summary_table = ['--------CELL SUMMARY--------',
-                        f'| ploidy:    | {str(self.ploidy).ljust(12, " ")}|',
-                        f'| monosome:  | {str(self.monosome).ljust(12, " ")}|',
-                        f'| revertant: | {str(self.revertant).ljust(12, " ")}|',
-                        f'| age:       | {str(self.age).ljust(12, " ")}|',
+        elif self.trisome:
+            if self.decide_revert2():
+                h = daughter.become_revert2(gen)
+                self.become_tetrasome(gen, h)
+                event_revert2 = True
+
+
+        self.history.append({'gen':gen, 
+                            'homologs':self.get_homologs(), 
+                            'event_monosome':event_monosome, 
+                            'event_revert':event_revert, 
+                            'event_revert2':event_revert2})
+        return daughter, event_monosome, event_revert, event_revert2
+        
+    def summary_print(self):
+        summary_table = ['--------CELL SUMMARY---------',
+                        f'| ploidy:     | {str(self.ploidy).ljust(12, " ")}|',
+                        f'| monosome:   | {str(self.monosome).ljust(12, " ")}|',
+                        f'| trisome:    | {str(self.monosome).ljust(12, " ")}|',
+                        f'| revertant:  | {str(self.revertant).ljust(12, " ")}|',
+                        f'| revertant2: | {str(self.revertant2).ljust(12, " ")}|',
+                        f'| age:        | {str(self.age).ljust(12, " ")}|',
                          '--------CELL SUMMARY--------'
                         ]
         for l in summary_table:
             print(l)
+    
+    def summary(self):
+        L = {'uid':self.uid,
+            'ploidy':self.ploidy,
+            'age':self.age,
+            'monosome':self.monosome,
+            'trisome':self.trisome,
+            'revertant':self.revertant,
+            'revertant2':self.revertant2,
+            'mother':self.mother,
+            'daughters':';'.join([str(d) for d in self.daughters]),
+            'genealogy':self.genealogy}
+        return L
 
 class Population:
     
     def __init__(self, founder, target_div):
         self.founder = founder
         self.target_div = target_div
-        self.monosome_fitness = founder.monosome_fitness
-        self.monosome_rate = founder.monosome_rate
-        self.revert_rate = founder.revert_rate
+        self.w_mono = founder.w_mono
+        self.w_triso = founder.w_triso
+        self.mu_A = founder.mu_A
+        self.mu_R = founder.mu_R
+        self.mu_T = founder.mu_T
         self.ploidy = founder.ploidy
 
     def expand(self, verbose=False, cleanup=True):
@@ -115,6 +225,7 @@ class Population:
         gen = self.founder.born
         Events_monosome = []
         Events_revert = []
+        Events_revert2 = []
         time_init = time.time()
         
         while len_pop < self.target_div:
@@ -124,7 +235,7 @@ class Population:
             for c in pop:
                 if c.decide_divide():
                     uid += 1
-                    daughter, new_monosome, new_revert = c.divide(uid, gen)
+                    daughter, new_monosome, new_revert, new_revert2 = c.divide(uid, gen)
                     new_gen.append(daughter)
                     len_new_gen += 1
                     
@@ -132,6 +243,8 @@ class Population:
                         Events_monosome.append((uid, gen))
                     elif new_revert:
                         Events_revert.append((uid, gen))
+                    elif new_revert2:
+                        Events_revert2.append((uid, gen))
                     if len_pop + len_new_gen >= self.target_div:
                         break
                     
@@ -150,6 +263,7 @@ class Population:
         self.Population = Pop
         self.Events_monosome = Events_monosome
         self.Events_revert = Events_revert
+        self.Events_revert2 = Events_revert2
         self.compute_time = time_final-time_init
         self.Report = self.report()
 
@@ -158,9 +272,11 @@ class Population:
 
     def report(self):
         Report = {}
-        Report['monosome_fitness'] = self.monosome_fitness
-        Report['monosome_rate'] = self.monosome_rate
-        Report['revert_rate'] = self.revert_rate
+        Report['w_mono'] = self.w_mono
+        Report['w_triso'] = self.w_triso
+        Report['mu_A'] = self.mu_A
+        Report['mu_R'] = self.mu_R
+        Report['mu_T'] = self.mu_T
         Report['ploidy'] = self.ploidy
         Report['m_monosome'] = len(self.Events_monosome)
         Report['m_revert'] = len(self.Events_revert)
